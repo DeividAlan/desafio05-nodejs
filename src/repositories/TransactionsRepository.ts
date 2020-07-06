421415ab7b7a357995b228c351d7eb129436b30c
Deivid Alan <deividalan@hotmail.com>
2020-07-05 23:47:41 -0300

    Finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,13 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +22,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const ttIncome = this.transactions.reduce((total, next) => {
+      if (next.type === 'income') {
+        return total + next.value;
+      }
+      return total + 0;
+    }, 0);
+
+    const ttOutcome = this.transactions.reduce((total, next) => {
+      if (next.type === 'outcome') {
+        return total + next.value;
+      }
+      return total + 0;
+    }, 0);
+
+    const balance: Balance = {
+      income: ttIncome,
+      outcome: ttOutcome,
+      total: ttIncome - ttOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Transaction type is wrong');
+    }
+
+    if (value <= 0) {
+      throw Error('Value cannot be less than or equal to 0');
+    }
+
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('insufficient funds');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
